7e9ebde646d8f6eac5fe47eb43fd287d8c5e7a0d
Oleg Suchkov <2oleg.ru@gmail.com>
2020-07-04 17:02:03 +0300

    Update order tests

--- test/order-test.js
+++ test/order-test.js
@@ -1,37 +1,38 @@
-var chai = require("chai");
-var assert = chai.assert;
+const chai = require('chai');
 
-var {
+const { assert } = chai;
+
+const {
   takeOrder,
   refundOrder,
   listItems,
-  searchOrder
-} = require("../src/order");
+  searchOrder,
+} = require('../src/order');
 
-describe("order.js", function() {
-  describe("takeOrder", function() {
-    it.skip("should be a function", function() {
+describe('order.js', () => {
+  describe('takeOrder', () => {
+    it('should be a function', () => {
       assert.isFunction(takeOrder);
     });
 
-    it.skip("should add new orders to an order type list", function() {
-      var order1 = {
+    it('should add new orders to an order type list', () => {
+      const order1 = {
         orderNumber: 1,
-        item: "burger",
-        price: "8.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'burger',
+        price: '8.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var order2 = {
+      const order2 = {
         orderNumber: 2,
-        item: "blt sandwich",
-        price: "5.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'blt sandwich',
+        price: '5.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var deliveryOrders = [];
+      const deliveryOrders = [];
 
       takeOrder(order1, deliveryOrders);
       takeOrder(order2, deliveryOrders);
@@ -40,33 +41,32 @@ describe("order.js", function() {
       assert.equal(deliveryOrders[1], order2);
     });
 
-    it.skip("should add another order to an order type list", function () {
-
-      var order1 = {
+    it('should add another order to an order type list', () => {
+      const order1 = {
         orderNumber: 1,
-        item: "burger",
-        price: "8.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'burger',
+        price: '8.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var order2 = {
+      const order2 = {
         orderNumber: 2,
-        item: "blt sandwich",
-        price: "5.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'blt sandwich',
+        price: '5.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var order3 = {
+      const order3 = {
         orderNumber: 3,
-        item: "rueben",
-        price: "8.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'rueben',
+        price: '8.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var deliveryOrders = [order1];
+      const deliveryOrders = [order1];
 
       takeOrder(order2, deliveryOrders);
       takeOrder(order3, deliveryOrders);
@@ -76,40 +76,40 @@ describe("order.js", function() {
       assert.equal(deliveryOrders[2], order3);
     });
 
-    it.skip("should not be able to hold more than 3 orders at a time", function() {
-      var order1 = {
+    it('should not be able to hold more than 3 orders at a time', () => {
+      const order1 = {
         orderNumber: 1,
-        item: "burger",
-        price: "8.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'burger',
+        price: '8.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var order2 = {
+      const order2 = {
         orderNumber: 2,
-        item: "blt sandwich",
-        price: "5.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'blt sandwich',
+        price: '5.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var order3 = {
+      const order3 = {
         orderNumber: 3,
-        item: "rueben",
-        price: "8.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'rueben',
+        price: '8.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var order4 = {
+      const order4 = {
         orderNumber: 4,
-        item: "garden salad",
-        price: "6.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'garden salad',
+        price: '6.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var deliveryOrders = [order1, order2];
+      const deliveryOrders = [order1, order2];
 
       takeOrder(order3, deliveryOrders);
       takeOrder(order4, deliveryOrders);
@@ -117,110 +117,110 @@ describe("order.js", function() {
       assert.equal(deliveryOrders.length, 3);
       assert.deepEqual(deliveryOrders, [order1, order2, order3]);
     });
-  }); 
+  });
 
-  describe("refundOrder", function() {
-    it.skip("should be a function", function () {
+  describe('refundOrder', () => {
+    it('should be a function', () => {
       assert.isFunction(refundOrder);
     });
 
-    it.skip("should remove an order by order number", function() {
-      var order1 = {
+    it('should remove an order by order number', () => {
+      const order1 = {
         orderNumber: 1,
-        item: "burger",
-        price: "8.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'burger',
+        price: '8.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var order2 = {
+      const order2 = {
         orderNumber: 2,
-        item: "blt sandwich",
-        price: "5.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'blt sandwich',
+        price: '5.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var order3 = {
+      const order3 = {
         orderNumber: 3,
-        item: "rueben",
-        price: "8.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'rueben',
+        price: '8.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var deliveryOrders = [order1, order2, order3];
+      const deliveryOrders = [order1, order2, order3];
 
       refundOrder(1, deliveryOrders);
 
       assert.equal(deliveryOrders.length, 2);
-      assert.deepEqual(deliveryOrders, [order2, order3])
+      assert.deepEqual(deliveryOrders, [order2, order3]);
     });
   });
 
-  describe("listOrders", function() {
-    it.skip("should be a function", function () {
+  describe('listOrders', () => {
+    it('should be a function', () => {
       assert.isFunction(listItems);
     });
 
-    it.skip("should list out all of the order items", function() {
-      var order1 = {
+    it('should list out all of the order items', () => {
+      const order1 = {
         orderNumber: 1,
-        item: "burger",
-        price: "8.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'burger',
+        price: '8.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var order2 = {
+      const order2 = {
         orderNumber: 2,
-        item: "blt sandwich",
-        price: "5.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'blt sandwich',
+        price: '5.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var order3 = {
+      const order3 = {
         orderNumber: 3,
-        item: "rueben",
-        price: "8.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'rueben',
+        price: '8.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var deliveryOrders = [order1, order2, order3];
-      var items = listItems(deliveryOrders);
+      const deliveryOrders = [order1, order2, order3];
+      const items = listItems(deliveryOrders);
 
-      assert.deepEqual(items, "burger, blt sandwich, rueben");
+      assert.deepEqual(items, 'burger, blt sandwich, rueben');
     });
   });
 
-  describe("searchOrder", function() {
-    it.skip("should be a function", function () {
+  describe('searchOrder', () => {
+    it('should be a function', () => {
       assert.isFunction(searchOrder);
     });
 
-    it.skip("should tell us if an order is in the list", function() {
-      var order1 = {
+    it('should tell us if an order is in the list', () => {
+      const order1 = {
         orderNumber: 1,
-        item: "burger",
-        price: "8.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'burger',
+        price: '8.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var order2 = {
+      const order2 = {
         orderNumber: 2,
-        item: "blt sandwich",
-        price: "5.99",
-        orderType: "delivery",
-        status: "accepted"
+        item: 'blt sandwich',
+        price: '5.99',
+        orderType: 'delivery',
+        status: 'accepted',
       };
 
-      var deliveryOrders = [order1, order2];
+      const deliveryOrders = [order1, order2];
 
-      assert.equal(searchOrder(deliveryOrders, "burger"), true);
-      assert.equal(searchOrder(deliveryOrders, "sushi"), false);
+      assert.equal(searchOrder(deliveryOrders, 'burger'), true);
+      assert.equal(searchOrder(deliveryOrders, 'sushi'), false);
     });
   });
 });
