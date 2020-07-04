0683708a0a4e7296406cc5bb8fc9e420ad0fd7bc
Oleg Suchkov <2oleg.ru@gmail.com>
2020-07-04 17:26:45 +0300

    Complete restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,47 @@
+function createRestaurant(name) {
+  return {
+    name,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    },
+  };
+}
 
+function addMenuItem(restaurant, item) {
+  if (restaurant.menus[item.type].includes(item)) {
+    return;
+  }
+
+  restaurant.menus[item.type].push(item);
+}
+
+function removeMenuItem(restaurant, name, type) {
+  if (typeof type === 'undefined') {
+    return `Sorry, we don't sell ${name}, try adding a new recipe!`;
+  }
+
+  const result = [];
+  let message = '';
+
+  for (const item of restaurant.menus[type]) {
+    if (item.name !== name) {
+      result.push(item);
+    }
+
+    if (item.name === name) {
+      message += `No one is eating our ${name} - it has been removed from the ${type} menu!`;
+    }
+  }
+
+  restaurant.menus[type] = result;
+
+  return message;
+}
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
-}
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem,
+};
