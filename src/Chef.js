b7ef6cc9185c775f0624e3e91d93bebe9ca79665
Oleg Suchkov <2oleg.ru@gmail.com>
2020-07-04 17:36:33 +0300

    Complete Chef.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,20 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+
+  greetCustomer(customerName, isMorning = false) {
+    return isMorning
+      ? `Good morning, ${customerName}!`
+      : `Hello, ${customerName}!`;
+  }
+
+  checkForFood(food) {
+    if (this.restaurant.menus[food.type].includes(food)) {
+      return `Yes, we're serving ${food.name} today!`;
+    }
+    return `Sorry, we aren't serving ${food.name} today.`;
   }
 }
 
