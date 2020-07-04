938db3cf74430d350f85e1988ab49f5a410b94ea
Oleg Suchkov <2oleg.ru@gmail.com>
2020-07-04 17:00:29 +0300

    Finish meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,35 @@
-function nameMenuItem() {
-
+function nameMenuItem(item) {
+  return `Delicious ${item}`;
 }
 
+function createMenuItem(name, price, type) {
+  return { name, price, type };
+}
+
+function addIngredients(name, ingredients) {
+  if (ingredients.includes(name)) { return; }
+
+  ingredients.push(name);
+}
+
+function formatPrice(price) {
+  return `$${price}`;
+}
+
+
+function decreasePrice(price) {
+  return price * 0.9;
+}
+
+function createRecipe(title, ingredients, type) {
+  return { title, ingredients, type };
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
-}
-
-
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe,
+};
